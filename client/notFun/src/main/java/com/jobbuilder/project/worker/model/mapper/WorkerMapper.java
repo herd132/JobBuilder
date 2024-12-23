603910a73afb4c9ebb92f468ebe601444492b304
dyddns542@naver.com <dyddns542@naver.com>
2024-12-23 14:00:37 +0900

    점심먹고 머지 템플릿폴더생성, 회원가입중

--- client/notFun/src/main/java/com/jobbuilder/project/worker/model/mapper/WorkerMapper.java
+++ client/notFun/src/main/java/com/jobbuilder/project/worker/model/mapper/WorkerMapper.java
@@ -2,7 +2,26 @@ package com.jobbuilder.project.worker.model.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.jobbuilder.project.worker.model.dto.Worker;
+
 @Mapper
 public interface WorkerMapper {
+
+	// 근로자 로그인 서비스
+	Worker login(String memberEmail);
+
+	// 근로자 이메일 중복확인
+	int checkEmail(String memberEmail);
+
+	// 근로자 닉네임 중복확인
+	int checkNickname(String workerNickname);
+
+	// 근로자 회원가입
+	int signupMember(Worker inputMember);
+	
+	int signupWorker();
+
+	// 근로자 아이디 중복확인
+	int checkId(String workerId);
 
 }
