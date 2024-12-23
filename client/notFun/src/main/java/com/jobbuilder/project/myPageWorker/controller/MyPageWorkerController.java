9e93207094992865c37496abcde8a18cce20b9c4
herd132 <dydcksdlsla@naver.com>
2024-12-23 14:02:02 +0900

    20241223

--- client/notFun/src/main/java/com/jobbuilder/project/myPageWorker/controller/MyPageWorkerController.java
+++ client/notFun/src/main/java/com/jobbuilder/project/myPageWorker/controller/MyPageWorkerController.java
@@ -2,6 +2,7 @@ package com.jobbuilder.project.myPageWorker.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.jobbuilder.project.myPageWorker.model.service.MyPageWorkerService;
 
@@ -10,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("myPageWorker")
+@SessionAttributes({"loginMember"})
 @RequiredArgsConstructor
 @Slf4j
 public class MyPageWorkerController {
