51a04ba582f952253e11722fffac82ebdaf8d01f
dyddns542@naver.com <dyddns542@naver.com>
2024-12-30 12:53:51 +0900

    오전작업

--- client/notFun/src/main/java/com/jobbuilder/project/worker/api/KakaoUtil.java
+++ client/notFun/src/main/java/com/jobbuilder/project/worker/api/KakaoUtil.java
@@ -5,23 +5,21 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-@Configuration
+import org.springframework.stereotype.Component;
+@Component
 @PropertySource("classpath:/config.properties")
-public class KakaoApi {
-	@Value("${kakao.api_key}")
-	private String kakaoApiKey;
-
-	@Value("${kakao.redirect_uri}")
-	private String kakaoRedirectUri;
+public class KakaoUtil {
+	@Value("${spring.kakao.auth.client}")
+	    private String client;
+	@Value("${spring.kakao.auth.redirect}")
+	    private String redirect;
     
     //인가 코드를 받아서 accessToken을 반환
 	public String getAccessToken(String code){
-		
-		
-		return code;};
+		return code;}
     //accessToken을 받아서 UserInfo 반환
 	public HashMap<String, Object> getUserInfo(String accessToken) {
-		return null;};
+		return null;}
     //accessToken을 받아서 로그아웃 시키는 메서드
-	public void kakaoLogout(String accessToken) {};
+	public void kakaoLogout(String accessToken) {}
 }
