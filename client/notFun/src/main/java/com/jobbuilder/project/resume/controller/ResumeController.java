a3d3bd3a5284f533bf4ed330bb29268eb85fb743
cmhinst2 <cmhinst2@gmail.com>
2025-01-03 11:25:29 +0900

    import 추가

--- client/notFun/src/main/java/com/jobbuilder/project/resume/controller/ResumeController.java
+++ client/notFun/src/main/java/com/jobbuilder/project/resume/controller/ResumeController.java
@@ -15,14 +15,13 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.jobbuilder.project.resume.model.service.ResumeService;
-import com.jobbuilder.project.worker.model.dto.Worker;
-import com.siot.IamportRestClient.IamportClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jobbuilder.project.resume.model.dto.CareerInfo;
+import com.jobbuilder.project.resume.model.dto.Resume;
+import com.jobbuilder.project.resume.model.dto.ResumeDaysTime;
 import com.jobbuilder.project.resume.model.service.ResumeService;
 import com.jobbuilder.project.worker.model.dto.Worker;
 
