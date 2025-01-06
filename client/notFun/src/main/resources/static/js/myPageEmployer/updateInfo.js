7b3417f83f30153bc9d0f0b88c1c9a5d4bcb6b4c
Wook-J <dnrwo0827@naver.com>
2025-01-06 10:01:47 +0900

    욱재

--- client/notFun/src/main/resources/static/js/myPageEmployer/updateInfo.js
+++ client/notFun/src/main/resources/static/js/myPageEmployer/updateInfo.js
@@ -18,14 +18,23 @@ const confirmPw = async () => {
   // 응답 상태가 204 (No Content)인지 확인
   if (resp.status === 204) {
     console.log("No content returned from server.");
-    wrongPwMessage.innerText = "Invalid credentials or no data found.";
+    wrongPwMessage.innerText = "일치하지 않는 비밀번호 입니다.";
     return;
   }
 
   const employer = await resp.json();
   
   myPageEmpInfoWhite.innerHTML = "일치한 회원입니다";
-  console.log(employer);
-  
-
+  console.log(employer);  
 }
+
+memberPw.addEventListener("input",()=>{
+  wrongPwMessage.innerText = "";
+})
+
+document.addEventListener('keyup', function(event) {
+  // Enter 키의 keyCode는 13
+  if (event.key === 'Enter') {
+    confirmPw(); // Enter 키에서 손을 뗄 때 confirmPw 함수 실행
+  }
+});
